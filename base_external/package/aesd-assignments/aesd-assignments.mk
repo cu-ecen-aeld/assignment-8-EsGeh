9a1528787840a329abdd91f80af02be3abc7d582
EsGeh <samuel@esgeh.net>
2025-06-04 21:47:41 +0200

    update buildroot external and publish qemu ports

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 # Fill up the contents below in order to reference your assignment 3 git contents:
-AESD_ASSIGNMENTS_VERSION = 'bc1c831337fda7d77006d6a1720bd32a1edeabad'
+AESD_ASSIGNMENTS_VERSION = 'db3039e6d80cda90a83f6fc832b2c86831fd4578'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,9 +16,10 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+# add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
@@ -27,6 +28,12 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 		$(@D)/finder-app/finder.sh \
 		$(@D)/finder-app/finder-test.sh \
 		$(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 \
+		$(@D)/server/aesdsocket \
+		$(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 \
+		$(@D)/server/aesdsocket-start-stop \
+		$(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
 
