1e6158a4093f2fad90b0291ac55bd6d1983399de
Samuel Gfrörer <samuel@esgeh.net>
2025-07-06 12:35:18 +0200

    aesd external uses ssh instead of https for git download

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = 'db3039e6d80cda90a83f6fc832b2c86831fd4578'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'https://github.com/cu-ecen-aeld/assignments-3-and-later-EsGeh'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-EsGeh.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -21,7 +21,7 @@ endef
 
 # add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 \
 		$(@D)/finder-app/writer \
@@ -31,6 +31,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 \
 		$(@D)/server/aesdsocket \
 		$(TARGET_DIR)/usr/bin
+	$(INSTALL) -d $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -m 0755 \
 		$(@D)/server/aesdsocket-start-stop \
 		$(TARGET_DIR)/etc/init.d/S99aesdsocket
